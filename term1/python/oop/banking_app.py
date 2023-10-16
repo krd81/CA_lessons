5ca75a8b5e47781671ece6974221bed08c5b9be1
Kelly <kelly.degannes@gmail.com>
2023-10-16 22:25:49 +1100

    Banking app with exception handling added

--- term1/python/oop/banking_app.py
+++ term1/python/oop/banking_app.py
@@ -3,13 +3,12 @@ import account
 acc1 =  account.Account('John Smith', 1000)
 acc2 =  account.Account('Mary Jones', 500)
 
-# acc1.get_balance()
-# acc2.get_balance()
-# acc1.deposit(100)
-# acc2.deposit(200)
-# acc1.deposit(50)
+acc1.get_balance()
+acc2.get_balance()
+acc1.deposit(100)
+acc2.deposit(200)
+acc1.deposit(50)
 
-# acc2.withdraw(800)
 
 # Transfer goes through
 try:
