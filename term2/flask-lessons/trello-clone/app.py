242f61a355661df918456ccce624d584a424065d
Kelly <kelly.degannes@gmail.com>
2023-11-23 17:06:00 +1100

    Moved authorisation check to separate function

--- term2/flask-lessons/trello-clone/app.py
+++ term2/flask-lessons/trello-clone/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
@@ -45,6 +45,24 @@ class User(db.Model):
 class UserSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'email', 'password', 'is_admin')
+
+
+def admin_required():
+     # Pseudo code to check is the user is an admin
+    # 1. Get the identity of the user via the token
+    user_email = get_jwt_identity()
+    # 2. Compare user's email against the database
+    stmt = db.select(User).where(User.email == user_email)
+    # 3.Create instance of user from the user retrieved from the database
+    user = db.session.scalar(stmt)
+    # 4. If user is not an admin, an error will be returned to the client
+    if not user.is_admin:
+        return abort(401)
+
+@app.errorhandler(401)
+def unauthorised(err):
+    return {'error': "You are not authorised to access this resource"}
+
 
 @app.cli.command('db_create')
 def db_create():
@@ -159,18 +177,7 @@ def login():
 @app.route('/cards')
 @jwt_required()
 def all_cards():
-    # Pseudo code to check is the user is an admin
-    # 1. Get the identity of the user via the token
-    user_email = get_jwt_identity()
-    # 2. Compare user's email against the database
-    stmt = db.select(User).where(User.email == user_email)
-    # 3.Create instance of user from the user retrieved from the database
-    user = db.session.scalar(stmt)
-
-    if not user.is_admin:
-        return {'error' : 'You must be an admin'}, 401
-
-
+    admin_required()
     # select * from cards;
     stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 0)).order_by(Card.title.desc())
     cards = db.session.scalars(stmt).all()
