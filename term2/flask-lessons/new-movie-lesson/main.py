b9edaf29ee22682d67015fb424280d64a9b842b8
Kelly <kelly.degannes@gmail.com>
2023-11-28 17:55:24 +1100

    Set up new movie app files/folders

---
+++ term2/flask-lessons/new-movie-lesson/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.app_config')
+    db = SQLAlchemy(app)
+    return app
+
+'''
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+'''
