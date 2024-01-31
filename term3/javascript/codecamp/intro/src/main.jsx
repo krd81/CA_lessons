34134c805504e03951ea6c426d2ba5fe9bed13ca
Kelly <kelly.degannes@gmail.com>
2024-01-31 20:49:15 +1100

    Code Camp Tutorial: cat contact page

---
+++ term3/javascript/codecamp/intro/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+
+)
+
+// function MyApp() {
+//   return (
+//     <ul>
+//       <li>1</li>
+//       <li>2</li>
+//       <li>3</li>
+//     </ul>
+//   )
+// }
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <MyApp />
+  
+// )
